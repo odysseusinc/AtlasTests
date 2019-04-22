bfbc4b978387307d45a56885e08e1088f2c11a28
ANemnyugin <anton.nemnyugin@firstlinesoftware.com>
2019-04-22 17:52:04 +0300

    Cohort Pathway rel1

--- src/test/java/odysseusAT/IncidenceRatesStepDefs.java
+++ src/test/java/odysseusAT/IncidenceRatesStepDefs.java
@@ -2,13 +2,17 @@ package odysseusAT;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
 
 public class IncidenceRatesStepDefs {
+    private String generatedString;
+    private String newGeneratedString;
     @Then("^can see Incidence Rates page$")
     public void canSeeIncidenceRatesPage() {
         $(By.xpath("//*[@data-bind='text: title']")).waitUntil(visible, 4000);
@@ -25,5 +29,93 @@ public class IncidenceRatesStepDefs {
     @Then("^can see Incidence Rates creation page$")
     public void canSeeIncidenceRatesCreationPage() {
         $(By.xpath("//*[@data-bind='text: title']")).shouldHave(text("New Incidence Rate Analysis"));
+    }
+
+    @When("^enter name on new Incidence Rates$")
+    public void enterNameOnNewIncidenceRates() {
+        generatedString = RandomStringUtils.randomAlphanumeric(10);
+        $(By.xpath("//*[@type='text']")).clear();
+        $(By.xpath("//*[@type='text']")).setValue(generatedString);
+    }
+
+    @When("^click to save Incidence Rates Button$")
+    public void clickToSaveIncidenceRatesButton() {
+        $(By.xpath("//*[@class='fa fa-save']")).click();
+    }
+
+    @Then("^can see new buttons in incidence rates field$")
+    public void canSeeNewButtonsInIncidenceRatesField() throws InterruptedException {
+        Thread.sleep(1500);
+        $(By.xpath("//*[@title='Close']")).shouldBe(visible);
+        $(By.xpath("//*[@title='Create a copy']")).shouldBe(visible);
+        $(By.xpath("//*[@title='Delete']")).shouldBe(visible);
+    }
+
+    @When("^click to cancel button in Incidence rates$")
+    public void clickToCancelButtonInIncidenceRates() {
+        $(By.xpath("//*[@title='Close']")).click();
+    }
+
+    @When("^enter name of our IR in filter$")
+    public void enterNameOfOurIRInFilter() {
+        $(By.xpath("//*[@type='search']")).waitUntil(visible,3000);
+        $(By.xpath("//*[@type='search']")).setValue(generatedString);
+    }
+
+    @Then("^can see our incidence rate in table$")
+    public void canSeeOurIncidenceRateInTable() {
+        $(By.xpath("//tbody/tr/td[2]")).shouldHave(text(generatedString));
+    }
+
+    @When("^click to our incidence rate$")
+    public void clickToOurIncidenceRate() {
+        $(By.xpath("//tbody/tr/td[2]")).click();
+    }
+
+    @Then("^can see our incidence rate$")
+    public void canSeeOurIncidenceRate() {
+        $(By.xpath("//*[@data-bind='text: title']")).shouldHave(text("Incidence Rate Analysis #"));
+    }
+
+    @When("^enter new name of incidence rate$")
+    public void enterNewNameOfIncidenceRate() {
+        newGeneratedString = RandomStringUtils.randomAlphanumeric(10);
+        $(By.xpath("//*[@type='text']")).clear();
+        $(By.xpath("//*[@type='text']")).setValue(newGeneratedString);
+
+    }
+
+    @When("^enter name of new IR in filter$")
+    public void enterNameOfNewIRInFilter() {
+        $(By.xpath("//*[@type='search']")).waitUntil(visible,3000);
+        $(By.xpath("//*[@type='search']")).setValue(newGeneratedString);
+    }
+
+    @Then("^can see new incidence rate in table$")
+    public void canSeeNewIncidenceRateInTable() {
+        $(By.xpath("//tbody/tr/td[2]")).shouldHave(text(newGeneratedString));
+    }
+
+    @When("^click to new incidence rate$")
+    public void clickToNewIncidenceRate() {
+        $(By.xpath("//tbody/tr/td[2]")).click();
+    }
+
+    @When("^click to delete IR button$")
+    public void clickToDeleteIRButton() {
+        $(By.xpath("//*[@title='Delete']")).waitUntil(visible,4000);
+        $(By.xpath("//*[@title='Delete']")).click();
+    }
+
+    @When("^accept delete IR alert$")
+    public void acceptDeleteIRAlert() {
+        switchTo().alert().accept();
+    }
+
+    @Then("^cant find IR in table$")
+    public void cantFindIRInTable() {
+        $(By.xpath("//*[@type='search']")).waitUntil(visible,3000);
+        $(By.xpath("//*[@type='search']")).setValue(newGeneratedString);
+        $(By.xpath("//tbody/tr/td[2]")).shouldNotHave(text(newGeneratedString));
     }
 }
--- src/test/resources/odysseusAT/checkIncidentRates.feature
+++ src/test/resources/odysseusAT/checkIncidentRates.feature
@@ -14,9 +14,12 @@ Feature: Incident rates check
     Then can see our incidence rate in table
     When click to our incidence rate
     Then can see our incidence rate
-    When enter new name of oe incidence rate
+    When enter new name of incidence rate
     When click to save Incidence Rates Button
-    Then can see name of our Incidence Rate
+    When click to cancel button in Incidence rates
+    When enter name of new IR in filter
+    Then can see new incidence rate in table
+    When click to new incidence rate
     When click to delete IR button
     When accept delete IR alert
     Then cant find IR in table
@@ -36,3 +39,29 @@ Feature: Incident rates check
     Then can see Generation pagein IR
     When click to Utilities page in IR
     Then can see Utilities page in IR
+
+  Scenario: Create IR as Angioedema across levetriacetam new users
+    When login to ATLAS as QA
+    When click to Incidence Rates menu as QA
+    Then can see Incidence Rates creation page
+    When enter name on new Incidence Rates
+    When click to save Incidence Rates Button
+    When click to add Target Cohort
+    When enter "New users of phenytoin" in filter of cohort
+    When click to result in IR
+    When enter "New users of levetiracetam" in filter of cohort
+    When click to result in IR
+    When close Cohort window in IR
+    Then can see new rows under Target Cohorts
+    When click to Add Outcome Cohort
+    When enter "Levetriacetam vs phenytoin outcome cohort" in filter of cohort
+    When click to result in IR
+    When close Cohort window in IR
+    Then can see new rows under Outcome Cohort
+    When choose Time at risk starts with value 14
+    When choose Time at risk ends with value 21
+    When click to Concept Sets tab
+    When press Load Concept Set in IR
+    When enter "Angioedema or Depression" in filter Concept Set in IR
+    When click to result in CS in IR
+    Then can see chosen concept set in table
