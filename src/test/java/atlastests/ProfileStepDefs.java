7f89bb99702a321ca63df7b4d1c97672b754bfe4
KSadomtsev <Kirill.Sadomtsev@odysseusinc.com>
2019-10-08 14:27:01 +0300

    @profile test pack has been refactored

--- src/test/java/atlastests/ProfileStepDefs.java
+++ src/test/java/atlastests/ProfileStepDefs.java
@@ -1,7 +1,5 @@
 package atlastests;
 
-import com.codeborne.selenide.Selectors;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
@@ -15,36 +13,32 @@ import static com.codeborne.selenide.Selenide.$$;
 public class ProfileStepDefs {
     @Then("^can see Profiles page$")
     public void canSeeProfilesPage() {
-        $(By.xpath("//*[@data-bind='text: title']")).waitUntil(visible,3000);
-        $(By.xpath("//*[@data-bind='text: title']")).shouldHave(text("Profiles"));
-
+        $(By.xpath("//*[@data-bind='text: title']")).waitUntil(visible,3000).shouldHave(text("Profiles"));
     }
 
     @When("^click to Select a Data Source$")
-    public void clickToSelectADataSource() throws InterruptedException {
-        Thread.sleep(2000);
-        $$(By.xpath("//*[@class='input-group-btn']/button")).get(0).click();
+    public void clickToSelectADataSource() {
+        $$(By.xpath("//*[@class='input-group-btn']/button")).get(0).waitUntil(visible, 5000).click();
 
     }
 
     @When("^choose \"([^\"]*)\" in Profile Source$")
-    public void chooseInProfileSource(String arg0) throws Throwable {
+    public void chooseInProfileSource(String arg0) {
         $(byText(arg0)).click();
     }
 
     @When("^enter (\\d+) in Profile Id and press Enter$")
     public void enterInProfileIdAndPressEnter(int arg0) {
-        $(By.xpath("//*[@class='form-control']")).setValue(String.valueOf(arg0));
-        $(By.xpath("//*[@class='form-control']")).pressEnter();
+        $(By.xpath("//*[@class='form-control']")).setValue(String.valueOf(arg0)).pressEnter();
     }
 
     @Then("^can see overlay$")
     public void canSeeOverlay() {
-        $(By.xpath("//*[@class='overlay']")).waitUntil(visible,60000);
+        $(By.xpath("//*[@class='overlay']")).waitUntil(visible,90000);
     }
 
     @Then("^can see table$")
     public void canSeeTable() {
-        $(By.xpath("//*[@class='sorting'][3]")).waitUntil(visible,10000);
+        $(By.xpath("//*[@class='sorting'][3]")).waitUntil(visible,90000);
     }
 }
