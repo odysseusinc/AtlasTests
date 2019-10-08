a0bf2e683c919b3862de3db32d3e493296f6071c
KSadomtsev <Kirill.Sadomtsev@odysseusinc.com>
2019-10-08 13:51:11 +0300

    @search test pack has been refactored

--- src/test/java/atlastests/SearchDefs.java
+++ src/test/java/atlastests/SearchDefs.java
@@ -1,24 +1,22 @@
 package atlastests;
 
-import com.codeborne.selenide.Selenide;
-import cucumber.api.PendingException;
+import com.codeborne.selenide.Condition;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.Objects;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
-public class SearchDefs  {
+public class SearchDefs {
     @When("^enter in search panel \"([^\"]*)\"$")
-    public void enterInSearchPanel(String arg0) throws Throwable {
-        $(By.xpath("//*[@placeholder='Type your search here']")).waitUntil(visible,3000);
-        $(By.xpath("//*[@placeholder='Type your search here']")).setValue(arg0);
+    public void enterInSearchPanel(String arg0) {
+        $(By.xpath("//*[@placeholder='Type your search here']")).waitUntil(visible, 10000).setValue(arg0);
     }
 
     @When("^search activated$")
@@ -29,7 +27,7 @@ public class SearchDefs  {
 
     @Then("^can see search result table$")
     public void canSeeSearchResultTable() {
-        $(By.xpath("//*[@class='conceptTable stripe compact hover dataTable no-footer']")).waitUntil(visible,25000);
+        $(By.xpath("//*[@class='conceptTable stripe compact hover dataTable no-footer']")).waitUntil(visible, 120000);
         $(By.xpath("//*[@class='fa fa-shopping-cart']")).shouldBe(visible);
         $(By.xpath("//*[@data-bind='template: { name: $component.headersTemplateId }']/tr/th[2]")).shouldHave(text("Id"));
         $(By.xpath("//*[@data-bind='template: { name: $component.headersTemplateId }']/tr/th[3]")).shouldHave(text("Code"));
@@ -44,21 +42,19 @@ public class SearchDefs  {
         $(By.xpath("//*[@data-dt-idx='2']")).shouldBe(text("2"));
 
 
-
     }//*[@id="DataTables_Table_8_wrapper"]/div/div[1]/div[1]/div[1]/div/div[2]/button[6]
 
     @When("^click to Column visibility$")
-    public void clickToColumnVisibility() throws InterruptedException {
+    public void clickToColumnVisibility() {
         $(By.xpath("//*[@class='dt-button buttons-collection buttons-colvis']")).click();
-        $(By.xpath("//*[@class='dt-button-collection']")).waitUntil(visible,3000);
+        $(By.xpath("//*[@class='dt-button-collection']")).waitUntil(visible, 3000);
 
 
     }
 
     @When("^click to button Standard Concept Caption$")
-    public void clickToButtonStandardConceptCation() throws InterruptedException {
-        $(By.xpath("//*[@data-cv-idx='5']")).waitUntil(visible,3000);
-        $(By.xpath("//*[@data-cv-idx='5']")).click();
+    public void clickToButtonStandardConceptCation() {
+        $(By.xpath("//*[@data-cv-idx='5']")).waitUntil(visible, 3000).click();
         $(By.xpath("//*[@class='dt-button-background']")).click();
 
 
@@ -66,8 +62,8 @@ public class SearchDefs  {
 
     @Then("^Standard Concept Caption should be shown$")
     public void standardConceptCationShouldBeShown() {
-        $(By.xpath("//*[@id='DataTables_Table_2']/thead/tr/th[6]")).waitUntil(visible,4000);
-        $(By.xpath("//*[@id='DataTables_Table_2']/thead/tr/th[6]")).shouldHave(text("Standard Concept Caption"));
+        $(By.xpath("//*[@id='DataTables_Table_2']/thead/tr/th[6]")).waitUntil(visible, 4000).
+                shouldHave(text("Standard Concept Caption"));
     }
 
     @When("^click to Id header$")
@@ -81,7 +77,7 @@ public class SearchDefs  {
         String value1 = $(By.xpath("//*[@id='DataTables_Table_2']/tbody/tr[1]/td[2]")).getText();
         String value2 = $(By.xpath("//*[@id='DataTables_Table_2']/tbody/tr[2]/td[2]")).getText();
 
-        Assert.assertTrue(Integer.parseInt(value1)<= Integer.parseInt(value2));
+        Assert.assertTrue(Integer.parseInt(value1) <= Integer.parseInt(value2));
 
     }
 
@@ -90,43 +86,36 @@ public class SearchDefs  {
         String value1 = $(By.xpath("//*[@id='DataTables_Table_2']/tbody/tr[1]/td[2]")).getText();
         String value2 = $(By.xpath("//*[@id='DataTables_Table_2']/tbody/tr[2]/td[2]")).getText();
 
-        Assert.assertTrue(Integer.parseInt(value1)>= Integer.parseInt(value2));
+        Assert.assertTrue(Integer.parseInt(value1) >= Integer.parseInt(value2));
     }
 
     @When("^enter in filtering search area \"([^\"]*)\"$")
-    public void enterInFilteringSearchArea(String arg0) throws Throwable {
+    public void enterInFilteringSearchArea(String arg0) {
         $(By.xpath("//*[@type='search']")).setValue(arg0);
 
     }
 
     @Then("^can see search result only with \"([^\"]*)\"$")
-    public void canSeeSearchResultOnlyWith(String arg0) throws Throwable {
-        Thread.sleep(1500);
-        $(By.xpath("//*[@class='invalid non-standard']")).shouldHave(text(arg0));
+    public void canSeeSearchResultOnlyWith(String arg0) {
+        $(By.xpath("//*[@class='invalid non-standard']")).waitUntil(visible, 5000).shouldHave(text(arg0));
 
     }
 
     @When("^click to CSV button$")
-    public void clickToCSVButton() throws Exception {
+    public void clickToCSVButton() {
         $(By.xpath("//*[@class='dt-button buttons-csv buttons-html5']")).click();
-        Thread.sleep(2000);
-
-
     }
 
 
-
     public static boolean isFileDownloaded(String downloadPath, String fileName) {
-        boolean flag = false;
         File dir = new File(downloadPath);
         File[] dir_contents = dir.listFiles();
 
-        for (int i = 0; i < dir_contents.length; i++) {
-            if (dir_contents[i].getName().equals(fileName))
-                return flag=true;
+        for (File dir_content : Objects.requireNonNull(dir_contents)) {
+            if (dir_content.getName().equals(fileName))
+                return true;
         }
-
-        return flag;
+        return false;
     }
 
     @Then("^file download$")
@@ -140,12 +129,9 @@ public class SearchDefs  {
 
     }
 
-
-
     @Then("^open page with first of four tabs for \"([^\"]*)\"$")
-    public void openPageWithFourTabsFor(String arg0) throws Throwable {
-        $(By.xpath("//*[@id='currentComponent']/div[1]/span[2]")).waitUntil(visible,5000);
-        $(By.xpath("//*[@id='currentComponent']/div[1]/span[2]")).shouldHave(text(arg0));
+    public void openPageWithFourTabsFor(String arg0) {
+        $(By.xpath("//*[@id='currentComponent']/div[1]/span[2]")).waitUntil(visible, 5000).shouldHave(text(arg0));
         $(By.xpath("//*[@class='nav nav-tabs']/li/a")).shouldHave(text("Details"));
         $(By.xpath("//*[@data-bind='with: $root.currentConcept']/tbody/tr[1]/td[1]")).shouldHave(text("Concept Name"));
         $(By.xpath("//*[@data-bind='with: $root.currentConcept']/tbody/tr[2]/td[1]")).shouldHave(text("Domain Id"));
@@ -160,47 +146,36 @@ public class SearchDefs  {
 
     @Then("^check second of four tabs$")
     public void checkSecondOfFourTabs() {
-        $(By.xpath("//*[@class='nav nav-tabs']/li[2]/a")).shouldHave(text("Related Concepts"));
-        $(By.xpath("//*[@class='nav nav-tabs']/li[2]/a")).click();
-        $(By.xpath("//*[@id='DataTables_Table_4']/tbody/tr[1]/td[4]/a")).waitUntil(visible,35000);
+        $(By.xpath("//*[@class='nav nav-tabs']/li[2]/a")).shouldHave(text("Related Concepts")).click();
+        $(By.xpath("//*[@id='DataTables_Table_4']/tbody/tr[1]/td[4]/a")).waitUntil(visible, 35000);
     }
 
     @Then("^check third of four tabs$")
     public void checkThirdOfFourTabs() {
-        $(By.xpath("//*[@class='nav nav-tabs']/li[3]/a")).shouldHave(text("Hierarchy"));
-        $(By.xpath("//*[@class='nav nav-tabs']/li[3]/a")).click();
-        $(By.xpath("//*[@id='wrapperParents']/div[1]")).waitUntil(visible,14000);
-        $(By.xpath("//*[@id='wrapperParents']/div[1]")).shouldHave(text("Parents"));
+        $(By.xpath("//*[@class='nav nav-tabs']/li[3]/a")).shouldHave(text("Hierarchy")).click();
+        $(By.xpath("//*[@id='wrapperParents']/div[1]")).waitUntil(visible, 14000).shouldHave(text("Parents"));
     }
 
     @Then("^check fourth of four tabs$")
-    public void checkFourthOfFourTabs() throws InterruptedException {
-        Thread.sleep(2000);
-        $(By.xpath("//*[@class='nav nav-tabs']/li[4]/a")).shouldHave(text("Record Counts"));
-        $(By.xpath("//*[@class='nav nav-tabs']/li[4]/a")).click();
-        $(By.xpath("//*[@class='panel-heading']")).waitUntil(visible,180000);
-        $(By.xpath("//*[@class='panel-heading']")).shouldHave(text("Record Counts across Sources"));
-
+    public void checkFourthOfFourTabs() {
+        $(By.xpath("//*[@class='nav nav-tabs']/li[4]/a")).waitUntil(visible, 5000).
+                shouldHave(text("Record Counts")).click();
+        $(By.xpath("//*[@class='panel-heading']")).waitUntil(visible, 180000).
+                shouldHave(text("Record Counts across Sources"));
     }
-
-
 
     @Then("^can see not null values at first row in DC or RDC$")
     public void canSeeNotNullValuesAtFirstRowInDCOrRDC() {
-        String tempRC, tempDRC;
-        tempRC = $(By.xpath("//table/tbody/tr/td[6]")).getText();
-        tempDRC = $(By.xpath("//table/tbody/tr/td[7]")).getText();
-        tempRC = tempRC.replaceAll(",","");
-        tempDRC = tempDRC.replaceAll(",","");
+        String tempRC = $(By.xpath("//table/tbody/tr/td[6]")).getText().replaceAll(",", "");
+        String tempDRC = $(By.xpath("//table/tbody/tr/td[7]")).getText().replaceAll(",", "");
+
         Assert.assertTrue(Integer.parseInt(tempRC) > 0);
         Assert.assertTrue(Integer.parseInt(tempDRC) > 0);
     }
 
     @When("^double click RC column$")
-    public void doubleClickRCColumn() throws InterruptedException {
-        Thread.sleep(1500);
-        $(By.xpath("//*[@class='numeric sorting']")).click();
-        Thread.sleep(1500);
-        $(By.xpath("//*[@class='numeric sorting_asc']")).click();
+    public void doubleClickRCColumn() {
+        $(By.xpath("//*[@class='numeric sorting']")).waitUntil(Condition.enabled, 5000).click();
+        $(By.xpath("//*[@class='numeric sorting_asc']")).waitUntil(Condition.enabled, 5000).click();
     }
 }
